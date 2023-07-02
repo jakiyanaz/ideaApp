029923eadbc7614bfe52777eb4d89ee291cb62ed
Jakiya Naz <nazjakiya1234@gmail.com>
2023-07-02 17:54:47 +0530

    bcrypt

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const serverconfig = require('./configs/server.config')
 const mongoose = require('mongoose')
-const dbConfig = require('./configs/db.config')
+const dbConfig = require('./configs/db.config');
+const userModel = require("./models/user.model");
+const bcrypt = require('bcrypt')
 
 const app = express()
 
@@ -15,7 +17,30 @@ db.on("error", ()=>{
 
 db.once("open", ()=>{
     console.log("DB is connected")
+
+    init();
 });
+
+async function init(){
+
+    let admin = await userModel.findOne({
+        userId : "admin"
+    })
+    
+    if(admin){
+        console.log("Admin user exists")
+        return;
+    }
+
+    admin = await userModel.create({
+        name : "Jakiya Naz",
+        userId : "admin",
+        email : "nazjakiya1234@gmail.com",
+        userType : "ADMIN",
+        password : bcrypt.hashSync("welcome1", 8)
+    });
+    console.log(admin);
+}
 
 
 app.listen(serverconfig.PORT, ()=>{
