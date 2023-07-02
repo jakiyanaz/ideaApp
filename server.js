65377e6d4a8415788b4f785a20844cd50be826bc
Jakiya Naz <nazjakiya1234@gmail.com>
2023-07-02 16:06:20 +0530

    user model

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const serverconfig = require('./configs/server.config')
+const mongoose = require('mongoose')
+const dbConfig = require('./configs/db.config')
 
 const app = express()
+
+
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection ;
+
+db.on("error", ()=>{
+    console.log("Error while connecting to db")
+});
+
+db.once("open", ()=>{
+    console.log("DB is connected")
+});
+
 
 app.listen(serverconfig.PORT, ()=>{
     console.log("Server Started")
